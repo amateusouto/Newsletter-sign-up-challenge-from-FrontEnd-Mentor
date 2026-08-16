583eb6f04a2268aca20fdd399774c05317ff4d78
Alberto Mateu <amateusouto@gmail.com>
2026-08-16 13:52:08 +0200

    mejoras en el codigo para los estados active

--- main.js
+++ main.js
@@ -3,29 +3,29 @@ const form = document.querySelector(".nl-form");
 const emailInput = document.getElementById("email");
 const successCard = document.querySelector(".success");
 const dismissBtn = document.getElementById("dismiss");
-const error = document.getElementById("error");
+const error = document.querySelector(".error-message");
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 
     const emailValue = emailInput.value.trim();
-    if (emailValue !== '' && emailValue.includes('@')) {
-        newsletter.style.display = "none"; 
-        successCard.style.display = "flex";
-        error.style.display = 'none';
+    if (emailInput.checkValidity()) {
+        newsletter.classList.add('hidden');
+        successCard.classList.add('active');
+        error.classList.remove('active');
         emailInput.classList.remove('error-input');
     } else {
-        error.style.display = 'block';
+        error.classList.add('active');
         emailInput.classList.add('error-input');
     }
 });
 
 if (dismissBtn) {
     dismissBtn.addEventListener('click', () => {
-        successCard.style.display = 'none';
-        newsletter.style.display = 'flex';
+        newsletter.classList.remove('hidden');
+        successCard.classList.remove('active');
         emailInput.value = '';
-        error.style.display = 'none';
         emailInput.classList.remove('error-input');
+        error.classList.remove('active');
     });
 }
